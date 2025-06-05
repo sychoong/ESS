f4f0013f321eb0d1103d5770a584869a5ce79756
JensenChekCW <jensen.chekcw@finpoints.com>
2025-06-05 14:57:21 +0800

    fix: build error
    https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,8 +2,9 @@
 
 import { AUTH_COOKIE_NAME } from "@/util/constants";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Auth() {
+function Auth() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -53,7 +54,7 @@ export default function Auth() {
       <div className="flex justify-center mt-4">
         <button
           className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700 w-full"
-          onClick={ () => {
+          onClick={() => {
             const url = new URL(
               decodeURI(
                 "https://login.microsoftonline.com/69857f89-851d-4f06-8114-84e781ee13e2/saml2?SAMLRequest=rVNLj9owEL7vr0C5m5AXcSxAotAHEgUEaQ%2B9VI4z2bXk2Knt7LL%2Fvk7CLrSqONWXSOP5Hv4yMzO0Fg1ZtvZJHuFXC8Y%2BjNw510Ia0l%2FOvVZLoqjhhkhagyGWkdPy65aE4wlptLKKKeH9BbuPosaAtlzJAbZZz7397uN2%2F3mz%2B1lkcZTgNApYGGY4nmRZGmGaBjRNClxURTTFGcVVNUC%2FgzaOZ%2B452qFy0OqZl6B3TnXu5VSAtEyotrxoGdPCRhpLpXWoSZigyRRNknySkCQiCf4x9K1dFFxS25M%2FWdsY4vtCPXI5rjnTyqjKKim4hDFTtT%2FNcJJWOEM4CUoUV44TB0GMcAwpDgCCCEK%2FCyZ8d9nn9oHLksvH%2B3EVQ5MhX%2FL8gA77Uz6QLN9iXClp2hr0CfQzZ%2FDtuL16Zu79mgpEGz621zTGlA9%2B%2FAgHJSRVhYIqiVEcQ4loEYYoy8KAFZiFrIh8yoy36EVnHYr0MerF%2FxWpwdKSWjrzbzWuqg3pfupmfVCCs9e%2B3p1PStfU3o%2Bwq%2FASVX0rabqpMdbZ9N5ZlkKol5UGat3YWN2CN%2FL%2F0L7sB5T9trjILZztaKXqhmpuujGBM2X2EtM1qtv2lXCjf4RqcXc7GGFdnysf3OdF6bIbF2BOO9dUmkZpe8non%2BSDa%2F%2BO7cXD2%2FXt6i9%2BAw%3D%3D&RelayState=https%3A%2F%2Flocalhost%2Fauth%2F"
@@ -63,11 +64,19 @@ export default function Auth() {
             params.set("RelayState", `${process.env.NEXT_PUBLIC_BASE}/auth/`);
             url.search = params.toString();
             router.push(url.toString()); // Redirect to home page after saving cookie
-        }}
-      >
-        Magic Link
-      </button>
+          }}
+        >
+          Magic Link
+        </button>
+      </div>
     </div>
-    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Auth />
+    </Suspense>
   );
 }
